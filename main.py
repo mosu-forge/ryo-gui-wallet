b5bdc86503b23ac9e036b9c2c15152423175efd1
mosu <mosu.forge@protonmail.com>
2018-06-07 23:27:02 -0700

    moved js to separate file

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ file_hashes = [
         ('www/scripts/mustache.min.js', '3258bb61f5b69f33076dd0c91e13ddd2c7fe771882adff9345e90d4ab7c32426'),
         ('www/scripts/jquery.qrcode.min.js', 'f4ccf02b69092819ac24575c717a080c3b6c6d6161f1b8d82bf0bb523075032d'),
         ('www/scripts/utils.js', 'd0c6870ed19c92cd123c7443cb202c7629f9cd6807daed698485fda25214bdb4'),
+        ('www/scripts/main.js', '6586f671988625c2c8849ed80187fccc177a962638d2e08ad429023e350fb258'),
 
         ('www/css/structure.css', 'f12c21c50150c652202366801a74443d412caf87210103860f4257ede988f99a'),
         ('www/css/light-theme.css', '58ad4961791523bc259c4794ece88af47170db31eb00cc5f65e2a5289410b226'),
